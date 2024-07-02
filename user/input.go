2bd7de3c519d74e113328022c97d43b23289898e
Otniel Kevin Abiel <rarjen57@gmail.com>
2024-07-02 11:19:21 +0700

    update input.go

--- user/input.go
+++ user/input.go
@@ -6,3 +6,8 @@ type RegisterUserInput struct {
 	Email      string `json:"email" binding:"required,email"`
 	Password   string `json:"password" binding:"required,min=6"`
 }
+
+type LoginInput struct {
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required"`
+}
