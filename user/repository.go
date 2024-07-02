8c9b4b7bcb59d3b14944d858d865db941538acff
Otniel Kevin Abiel <rarjen57@gmail.com>
2024-07-02 16:05:10 +0700

    update repository.go

--- user/repository.go
+++ user/repository.go
@@ -5,6 +5,8 @@ import "gorm.io/gorm"
 type Repository interface {
 	Save(user User) (User, error)
 	FindByEmail(email string) (User, error)
+	FindById(id int) (User, error)
+	Update(user User) (User, error)
 }
 
 type repository struct {
@@ -37,6 +39,26 @@ func (r *repository) FindByEmail(email string) (User, error) {
 
 	return user, nil
 
+}
+
+func (r *repository) FindById(id int) (User, error) {
+	var user User
+
+	err := r.db.Where("id = ?", id).Find(&user).Error
+	if err != nil {
+		return user, err
+	}
+
+	return user, nil
+}
+
+func (r *repository) Update(user User) (User, error) {
+	err := r.db.Save(&user).Error
+	if err != nil {
+		return user, err
+	}
+
+	return user, nil
 }
 
 // Note :
