f9a3676fbd348089dc96f28d04921fef3a0a4eeb
Otniel Kevin Abiel <rarjen57@gmail.com>
2024-07-03 11:28:06 +0700

    update campaign.go

--- handler/campaign.go
+++ handler/campaign.go
@@ -1,1 +1,34 @@
 package handler
+
+import (
+	"bwa-golang/campaign"
+	"bwa-golang/helpers"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+type campaignHandler struct {
+	service campaign.Service
+}
+
+func NewCampaignHandler(service campaign.Service) *campaignHandler {
+	return &campaignHandler{service}
+}
+
+func (h *campaignHandler) GetCampaigns(c *gin.Context) {
+	userId, _ := strconv.Atoi(c.Query("user_id"))
+
+	campaigns, err := h.service.GetCampaigns(userId)
+
+	if err != nil {
+		response := helpers.ApiResponseBadRequest("Error to get campaigns", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	response := helpers.APIResponseSuccess("List of campaigns", campaigns)
+	c.JSON(http.StatusOK, response)
+
+}
