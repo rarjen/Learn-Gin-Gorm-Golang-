bb3a0228efe19628af27906aaba19597afb944a8
Otniel Kevin Abiel <rarjen57@gmail.com>
2024-07-03 16:36:40 +0700

    update campaign.go

--- handler/campaign.go
+++ handler/campaign.go
@@ -30,5 +30,26 @@ func (h *campaignHandler) GetCampaigns(c *gin.Context) {
 
 	response := helpers.APIResponseSuccess("List of campaigns", campaign.FormatCampaigns(data))
 	c.JSON(http.StatusOK, response)
+}
 
+func (h *campaignHandler) GetCampaign(c *gin.Context) {
+
+	var input campaign.GetCampaignDetailInput
+
+	err := c.ShouldBindUri(&input)
+	if err != nil {
+		response := helpers.ApiResponseBadRequest("Failed to get detail of campaign", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	campaignDetail, err := h.service.GetCampaignById(input)
+	if err != nil {
+		response := helpers.ApiResponseBadRequest("Failed to get detail of campaign", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	response := helpers.APIResponseSuccess("Failed to get detail of campaign", campaign.FormatCampaignDetail(campaignDetail))
+	c.JSON(http.StatusOK, response)
 }
