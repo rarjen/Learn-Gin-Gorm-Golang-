b3d9fb927dd5e5acae19fb0867907d1ab61ed4ff
Otniel Kevin Abiel <rarjen57@gmail.com>
2024-07-03 08:57:04 +0700

    update repository.go

--- campaign/repository.go
+++ campaign/repository.go
@@ -1,1 +1,40 @@
 package campaign
+
+import "gorm.io/gorm"
+
+type Repository interface {
+	// Karena ingin mengambil banya maka menggunakan slice
+	FindAll() ([]Campaign, error)
+	FindByUserID(userID int) ([]Campaign, error)
+}
+
+type repository struct {
+	db *gorm.DB
+}
+
+// Digunakan untuk inisialisasi instance baru dari struct repository dengan parameter db
+func NewRepository(db *gorm.DB) *repository {
+	return &repository{db}
+}
+
+func (r *repository) FindAll() ([]Campaign, error) {
+	var campaigns []Campaign
+
+	err := r.db.Find(&campaigns).Error
+	if err != nil {
+		return campaigns, err
+	}
+
+	return campaigns, nil
+}
+
+func (r *repository) FindByUserID(userID int) ([]Campaign, error) {
+	var campaigns []Campaign
+
+	err := r.db.Where("user_id = ?", userID).Find(&campaigns).Error
+	if err != nil {
+		return campaigns, err
+	}
+
+	return campaigns, nil
+}
