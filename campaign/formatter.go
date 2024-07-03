706b410712f1c41160d7d6a9cf1e929592456885
Otniel Kevin Abiel <rarjen57@gmail.com>
2024-07-03 13:23:19 +0700

    update formatter.go

--- campaign/formatter.go
+++ campaign/formatter.go
@@ -1,1 +1,43 @@
 package campaign
+
+type CampignFormatter struct {
+	ID               int    `json:"id"`
+	UserID           int    `json:"user_id"`
+	Name             string `json:"name"`
+	ShortDescription string `json:"short_description"`
+	ImageUrl         string `json:"image_url"`
+	GoalAmount       int    `json:"goal_amount"`
+	CurrentAmount    int    `json:"current_amount"`
+}
+
+func FormatCampign(campaign Campaign) CampignFormatter {
+	campaignFormatter := CampignFormatter{}
+	campaignFormatter.ID = campaign.ID
+	campaignFormatter.UserID = campaign.UserID
+	campaignFormatter.Name = campaign.Name
+	campaignFormatter.ShortDescription = campaign.ShortDescription
+	campaignFormatter.GoalAmount = campaign.GoalAmount
+	campaignFormatter.CurrentAmount = campaign.CurrentAmount
+
+	if len(campaign.CampaignImages) > 0 {
+		campaignFormatter.ImageUrl = campaign.CampaignImages[0].FileName
+	}
+
+	return campaignFormatter
+}
+
+func FormatCampaigns(campaigns []Campaign) []CampignFormatter {
+
+	if len(campaigns) == 0 {
+		return []CampignFormatter{}
+
+	}
+	var campaignsFormatter []CampignFormatter
+
+	for _, v := range campaigns {
+		campaignFormatter := FormatCampign(v)
+		campaignsFormatter = append(campaignsFormatter, campaignFormatter)
+	}
+
+	return campaignsFormatter
+}
