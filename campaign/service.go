dbbd89a1e311f1afceaf024b5e66eb6730ffe91a
Otniel Kevin Abiel <rarjen57@gmail.com>
2024-07-04 08:45:56 +0700

    update service.go

--- campaign/service.go
+++ campaign/service.go
@@ -1,8 +1,15 @@
 package campaign
 
+import (
+	"fmt"
+
+	"github.com/gosimple/slug"
+)
+
 type Service interface {
 	GetCampaigns(userId int) ([]Campaign, error)
 	GetCampaignById(input GetCampaignDetailInput) (Campaign, error)
+	CreateCampign(input CreateCampignInput) (Campaign, error)
 }
 
 type service struct {
@@ -38,4 +45,27 @@ func (s *service) GetCampaignById(input GetCampaignDetailInput) (Campaign, error
 	}
 
 	return campaign, nil
+}
+
+func (s *service) CreateCampign(input CreateCampignInput) (Campaign, error) {
+
+	campaign := Campaign{}
+
+	campaign.Name = input.Name
+	campaign.ShortDescription = input.ShortDescription
+	campaign.Description = input.Description
+	campaign.Perks = input.Perks
+	campaign.GoalAmount = input.GoalAmount
+	campaign.UserID = input.User.ID
+
+	slugCandidate := fmt.Sprintf("%s %d", input.Name, input.User.ID)
+	campaign.Slug = slug.Make(slugCandidate)
+
+	// pembuatan slug
+	newCampign, err := s.repository.Save(campaign)
+	if err != nil {
+		return newCampign, err
+	}
+
+	return newCampign, nil
 }
