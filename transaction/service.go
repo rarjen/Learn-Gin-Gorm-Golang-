12ed9231f66fbcdcbef274f2897d4419df9486a8
Otniel Kevin Abiel <rarjen57@gmail.com>
2024-07-07 22:03:03 +0700

    feat: create service.go

---
+++ transaction/service.go
@@ -0,0 +1,38 @@
+package transaction
+
+import (
+	"bwa-golang/campaign"
+	"errors"
+)
+
+type service struct {
+	repository         Repository
+	campaignRepository campaign.Repository
+}
+
+type Service interface {
+	GetTransactionsByCampaignId(input GetCampaignTransactionsInput) ([]Transaction, error)
+}
+
+func NewService(repository Repository, campaignRepository campaign.Repository) *service {
+	return &service{repository, campaignRepository}
+}
+
+func (s *service) GetTransactionsByCampaignId(input GetCampaignTransactionsInput) ([]Transaction, error) {
+
+	campaign, err := s.campaignRepository.FindByID(input.ID)
+	if err != nil {
+		return []Transaction{}, err
+	}
+
+	if campaign.UserID != input.User.ID {
+		return []Transaction{}, errors.New("not an owner of the campaign")
+	}
+
+	var campaignID = input.ID
+	transactions, err := s.repository.GetByCampaignID(campaignID)
+	if err != nil {
+		return transactions, err
+	}
+	return transactions, nil
+}
