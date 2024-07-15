97883d85d58553a89096ee4c0185afc709d5ddf4
Otniel Kevin Abiel <rarjen57@gmail.com>
2024-07-15 10:33:49 +0700

    update service.go

--- transaction/service.go
+++ transaction/service.go
@@ -2,21 +2,26 @@ package transaction
 
 import (
 	"bwa-golang/campaign"
+	"bwa-golang/helpers"
+	"bwa-golang/payment"
+
 	"errors"
 )
 
 type service struct {
 	repository         Repository
 	campaignRepository campaign.Repository
+	paymentService     payment.Service
 }
 
 type Service interface {
 	GetTransactionsByCampaignId(input GetCampaignTransactionsInput) ([]Transaction, error)
 	GetTransactionsByUserID(userID int) ([]Transaction, error)
+	CreateTransactions(input CrateTransactionInput) (Transaction, error)
 }
 
-func NewService(repository Repository, campaignRepository campaign.Repository) *service {
-	return &service{repository, campaignRepository}
+func NewService(repository Repository, campaignRepository campaign.Repository, paymentService payment.Service) *service {
+	return &service{repository, campaignRepository, paymentService}
 }
 
 func (s *service) GetTransactionsByCampaignId(input GetCampaignTransactionsInput) ([]Transaction, error) {
@@ -44,4 +49,20 @@ func (s *service) GetTransactionsByUserID(userID int) ([]Transaction, error) {
 		return transactions, err
 	}
 	return transactions, nil
+}
+
+func (s *service) CreateTransactions(input CrateTransactionInput) (Transaction, error) {
+	transaction := Transaction{}
+	transaction.CampaignID = input.CampaignId
+	transaction.Amount = input.Amount
+	transaction.UserID = input.User.ID
+	transaction.Status = "pending"
+	transaction.Code = "CODE - " + helpers.GenerateRandomString()
+
+	newTransaction, err := s.repository.Save(transaction)
+
+	if err != nil {
+		return newTransaction, err
+	}
+	return newTransaction, nil
 }
